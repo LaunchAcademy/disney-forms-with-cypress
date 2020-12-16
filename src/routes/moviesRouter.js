9eba96780305bf87ec764807b063ba45d3763ee1
dovakeen118 <kegillis118@gmail.com>
2020-12-16 18:28:17 -0500

    final code

--- src/routes/moviesRouter.js
+++ src/routes/moviesRouter.js
@@ -9,4 +9,24 @@ moviesRouter.get("/", (req, res) => {
   res.render("movies/index", { movies });
 });
 
+moviesRouter.get("/new", (req, res) => {
+  res.render("movies/new")
+})
+
+moviesRouter.post("/", (req, res) => {
+  const movieTitle = req.body.title
+  const movieReleaseYear = req.body.releaseYear
+  const movieRuntime = req.body.runtime
+
+  const newMovieObject = {title: movieTitle, releaseYear: movieReleaseYear, runtime: movieRuntime}
+
+  const newMovie = new Movie(newMovieObject)
+
+  if(newMovie.save()) {
+    res.redirect("/movies")
+  } else {
+    res.render("movies/new", { errors: newMovie.errors, movie: newMovie })
+  }
+})
+
 export default moviesRouter;
