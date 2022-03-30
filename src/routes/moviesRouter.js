6d03b318d98a659298d0730d30ac58bbca642114
ohad-porat <ohadp23@gmail.com>
2022-03-30 17:32:11 -0400

    boston 36 final code

--- src/routes/moviesRouter.js
+++ src/routes/moviesRouter.js
@@ -1,12 +1,34 @@
-import express from "express";
+import express from "express"
 
-import Movie from "../models/Movie.js";
+import Movie from "../models/Movie.js"
 
-const moviesRouter = new express.Router();
+const moviesRouter = new express.Router()
 
 moviesRouter.get("/", (req, res) => {
-  const movies = Movie.findAll();
-  res.render("movies/index", { movies });
-});
+  const movies = Movie.findAll()
+  res.render("movies/index", { movies })
+})
 
-export default moviesRouter;
+moviesRouter.get("/new", (req, res) => {
+  res.render("movies/new")
+})
+
+moviesRouter.post("/", (req, res) => {
+  const title = req.body.title
+  const releaseYear = req.body.releaseYear
+  const runtime = req.body.runtime
+
+  const newMovieObject = new Movie({
+    title: title,
+    releaseYear: releaseYear,
+    runtime: runtime,
+  })
+
+  if(newMovieObject.save()) {
+    res.redirect("/movies")
+  } else {
+    res.render("movies/new", {errors: newMovieObject.errors, movie: newMovieObject})
+  }
+})
+
+export default moviesRouter
