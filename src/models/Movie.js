6c6f878e4a13bfc51db4e09cb98ff025bfd5e2e4
Nick Alberts <nwalberts5@gmail.com>
2022-12-21 11:19:58 -0500

    finished

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,33 +1,33 @@
-import fs from "fs";
-import _ from "lodash";
+import fs from "fs"
+import _ from "lodash"
 
 const disneyMoviesPath = "disneyMovies.json"
 
 class Movie {
   constructor({ id, title, releaseYear, runtime }) {
-    this.id = id;
-    this.title = title;
-    this.releaseYear = releaseYear;
-    this.runtime = runtime;
+    this.id = id
+    this.title = title
+    this.releaseYear = releaseYear
+    this.runtime = runtime
   }
 
   // static or "class" method means we call it on the class, not on an instance
   static findAll() {
     // retrieve the JSON data, and turn it into JS (an object with an array)
     const moviesData = JSON.parse(fs.readFileSync(disneyMoviesPath))
-
+    
     // iterate over all of the movies, and ensure they are remade as Movie objects
     const movies = moviesData.disneyMovies.map((movie) => {
       // each `return` in .map creates a new element in the `movies` array
-      return new Movie(movie);
-    });
+      return new Movie(movie)
+    })
 
     return movies
   }
 
   isValid() {
-    // an empty errors object to add to
-    this.errors = {}
+    // an empty array to add to
+    this.errors = []
     // the fields we wish to check for
     const requiredFields = ["title", "releaseYear", "runtime"]
     // by default, we will assume the movie is valid
@@ -35,14 +35,12 @@ class Movie {
 
 
     for (const requiredField of requiredFields) {
-      // create a new key using the required field, that we can add errors to 
-      this.errors[requiredField] = []
       // if the field is not on the object we are calling `isValid` on
       if(!this[requiredField]) {
         // set `isValid` to false 
         isValid = false
         // and add an error message to the array
-        this.errors[requiredField].push("can't be blank")
+        this.errors.push(`${requiredField} can't be blank`)
       }
     }
     
@@ -56,9 +54,9 @@ class Movie {
     const maxMovie = _.maxBy(
       this.findAll(),
       (movie) => movie.id
-    );
+    )
     // add one, because this is going to be the new id of the new movie object
-    return maxMovie.id + 1;
+    return maxMovie.id + 1
   }
 
   save() {
@@ -66,16 +64,16 @@ class Movie {
     if (this.isValid()) {
       // delete all errors and grab the id for soon to be persisted movie
       delete this.errors
-      this.id = this.constructor.getNextMovieId();
+      this.id = this.constructor.getNextMovieId()
       
       // grab all of the existing movies into an array, and add this one to it
-      const movies = this.constructor.findAll();
-      movies.push(this);
+      const movies = this.constructor.findAll()
+      movies.push(this)
       
       // add all of these movies (including the new one) to the json file
-      const data = { disneyMovies: movies };
-      fs.writeFileSync(disneyMoviesPath, JSON.stringify(data));
-      return true;
+      const data = { disneyMovies: movies }
+      fs.writeFileSync(disneyMoviesPath, JSON.stringify(data))
+      return true
     } else {
       // if it isnt valid, we return false
       return false
@@ -83,4 +81,4 @@ class Movie {
   }
 }
 
-export default Movie;
+export default Movie
--- src/routes/moviesRouter.js
+++ src/routes/moviesRouter.js
@@ -6,7 +6,24 @@ const moviesRouter = new express.Router();
 
 moviesRouter.get("/", (req, res) => {
   const movies = Movie.findAll();
-  res.render("movies/index", { movies });
+  res.render("movies/index", { movies: movies });
 });
+
+moviesRouter.get("/new", (req, res) => {
+  res.render("movies/form")
+})
+
+moviesRouter.post("/", (req, res) => {
+  const movieParams = req.body
+
+  // movieParams.id = Movie.getNextMovieId()
+
+  const newMovie = new Movie(movieParams)
+  if (newMovie.save()){
+    res.redirect("/movies")
+  } else {
+    res.render("movies/form", { errors: newMovie.errors, movie: newMovie })
+  }
+})
 
 export default moviesRouter;
